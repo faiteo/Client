45819b22d34d886311b4ad9b8524376ebc32c4b1
Faith Nielsen <faiteo2002@yahoo.com>
2016-02-19 19:29:59 +0100

    Client ASP.Net Application for Product_Service_Project
    Client ASP.Net Application for Product_Service_Project

---
+++ ProductTestWebPage.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ProductTestWebPage.aspx.cs" Inherits="Client.ProductTestWebPage" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+
+        <asp:TextBox ID="txtBoxProdID" runat="server"></asp:TextBox><asp:Label ID="Label1" runat="server" Text="Enter Product ID"></asp:Label><br />
+        <br />
+        <asp:Button ID="btnFindProduct" runat="server" Text="Get Product Details" OnClick="btnFindProduct_Click" />
+        <br />
+        <br />
+      
+        <asp:Label ID="lblError" runat="server" Text=""></asp:Label>
+
+
+        <h2 runat="server" id="myHeader"><u>Product Information:</u></h2>
+        <asp:Panel ID="panelProdInfo" runat="server">
+
+            <table>
+
+                <tr>
+                    <td><b>Product ID: </b></td>
+                    <td><asp:Label ID="lblID" runat="server" Text=""></asp:Label></td>
+                </tr>
+                <tr>
+                    <td><b>Product Title: </b></td>
+                    <td><asp:Label ID="lblTitle" runat="server" Text=""></asp:Label></td>
+                </tr>
+                <tr>
+                    <td><b>Description: </b></td>
+                    <td><asp:Label ID="lblDescription" runat="server" Text=""></asp:Label></td>
+                </tr>
+
+                <tr>
+                    <td><b>Product Type: </b></td>
+                     <td><asp:Label ID="lblType" runat="server" Text=""></asp:Label></td>
+                </tr>
+
+                <tr>
+                    <td><b>Price: </b></td>
+                    <td><asp:Label ID="lblPrice" runat="server" Text=""></asp:Label> </td>
+                </tr>
+
+
+            </table>
+
+        </asp:Panel>
+    </form>
+</body>
+</html>
---
+++ ProductTestWebPage.aspx.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Client
+{
+    public partial class ProductTestWebPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            myHeader.Visible = false;
+            panelProdInfo.Visible = false;
+
+        }
+
+        protected void btnFindProduct_Click(object sender, EventArgs e)
+        {
+            ServiceReference_Product.ProductServiceClient client = new ServiceReference_Product.ProductServiceClient("BasicHttpBinding_IProductService");
+            int idInput;
+            bool res;
+            if (res = Int32.TryParse(txtBoxProdID.Text, out idInput))
+            {
+                ServiceReference_Product.Product prod = client.GetProduct(idInput);
+                if (prod != null)
+                {
+                    if (prod.Type == ServiceReference_Product.Product.ProductType.Book)
+                    {
+                        myHeader.Visible = true;
+                        panelProdInfo.Visible = true;
+
+                        ServiceReference_Product.Book book = (ServiceReference_Product.Book)prod;
+                        lblID.Text = book.Id.ToString();
+                        lblTitle.Text = book.Title;
+                        lblDescription.Text = book.Description;
+                        lblType.Text = book.Type.ToString();
+                        lblPrice.Text = book.Price.ToString() + " DKK";
+
+                        Label lblAuthor = new Label();
+                        lblAuthor.Font.Bold = true;
+                        lblAuthor.Text = "Author Name: ";
+
+                        panelProdInfo.Controls.Add(lblAuthor);
+
+                        Label authorContent = new Label();
+                        authorContent.Text = book.AuthorName;
+                        panelProdInfo.Controls.Add(authorContent);
+                    }
+                    else
+                    {
+                        myHeader.Visible = true;
+                        panelProdInfo.Visible = true;
+
+                        ServiceReference_Product.CD cdObject = (ServiceReference_Product.CD)prod;
+                        lblID.Text = cdObject.Id.ToString();
+                        lblTitle.Text = cdObject.Title;
+                        lblDescription.Text = cdObject.Description;
+                        lblType.Text = cdObject.Type.ToString();
+                        lblPrice.Text = cdObject.Price.ToString() + " DKK";
+
+                        Label lblArtistName = new Label();
+                        lblArtistName.Text = "Artist Name: ";
+                        lblArtistName.Font.Bold = true;
+                        panelProdInfo.Controls.Add(lblArtistName);
+
+                        Label lblAtristNameContent = new Label();
+                        lblAtristNameContent.Text = cdObject.ArtistName;
+                        panelProdInfo.Controls.Add(lblAtristNameContent);
+
+                        panelProdInfo.Controls.Add(new LiteralControl("<br />"));
+
+                        Label lblNumOfTracks = new Label();
+                        lblNumOfTracks.Font.Bold = true;
+                        lblNumOfTracks.Text = "Number of Tracks: ";
+                        panelProdInfo.Controls.Add(lblNumOfTracks);
+
+                        Label lblnumOfTracksContent = new Label();
+                        lblnumOfTracksContent.Text = cdObject.NumberOfTracks.ToString();
+                        panelProdInfo.Controls.Add(lblnumOfTracksContent);
+                    }
+                }
+                else
+                {
+                    lblError.Text = "Product IDs are 1,2,3,4,5,8,9,10,12 and 13";
+                }
+            }
+
+            else
+            {
+                lblError.Text = "Error ! Valid product IDs are: 1,2,3,4,5,8,9,10,12 and 13";
+            }
+        }
+
+
+    }
+}
